454f55377bbf155d06e744488c97005d3cb45415
1nsomnes <1nsomnes@users.noreply.github.com>
2025-06-22 09:57:51 -0700

    add in app playback controls

---
+++ lib/components/circle_buttons.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutter/material.dart';
+
+Widget buildCircleButton({
+  required IconData icon,
+  required VoidCallback onPressed,
+  Color backgroundColor = Colors.blue,
+  double size = 56,
+}) {
+  return RawMaterialButton(
+    onPressed: onPressed,
+    constraints: BoxConstraints.tightFor(
+      width: size,
+      height: size,
+    ),
+    shape: CircleBorder(),
+    fillColor: backgroundColor,
+    elevation: 2,
+    child: Icon(icon, color: Colors.white),
+  );
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:lightify/components/circle_buttons.dart';
 import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
 import 'package:lightify/providers/auth_provider.dart';
 import 'package:provider/provider.dart';
@@ -18,7 +19,10 @@ class _HomePageState extends State<HomePage> {
   Future<void> loadHtmlFromAssets(BuildContext context) async {
     String html = await rootBundle.loadString('assets/player.html');
     if (context.mounted) {
-      html = html.replaceAll("{token}", Provider.of<AuthProvider>(context, listen: false).getToken);
+      html = html.replaceAll(
+        "{token}",
+        Provider.of<AuthProvider>(context, listen: false).getToken,
+      );
       _controller.loadHtmlString(html);
     }
   }
@@ -53,26 +57,55 @@ class _HomePageState extends State<HomePage> {
     loadHtmlFromAssets(context);
   }
 
+  void _togglePlay() {
+    _controller.runJavaScript("togglePlayback();");
+  }
+
+  void _next() {
+    _controller.runJavaScript("next();");
+  }
+
+  void _prev() {
+    _controller.runJavaScript("previous();");
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      /*home: Scaffold(
-        body: Expanded(child: WebViewWidget(controller: _controller)),
-      ),*/
+      debugShowCheckedModeBanner: false,
+
       home: Center(
         child: Column(
           children: [
-            Expanded(child: WebViewWidget(controller: _controller)),
-            Padding(
-              padding: const EdgeInsets.all(8),
-              child: ElevatedButton(
-                onPressed: () {
-                  // 1) call the JS function with an argument
-                  //_controller.runJavaScript("sayHello('Flutter Dev');");
-                  _controller.runJavaScript("togglePlayback();");
-                },
-                child: const Text('Call JS sayHello()'),
-              ),
+            SizedBox.fromSize(
+              size: Size.zero,
+              child: WebViewWidget(controller: _controller),
+            ),
+            Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                buildCircleButton(
+                  icon: Icons.skip_previous,
+                  onPressed: _prev,
+                  size: 40,
+                  backgroundColor: Colors.grey[700]!,
+                ),
+                SizedBox(width: 20),
+                buildCircleButton(
+                  icon: Icons.play_arrow,
+                  onPressed: _togglePlay,
+                  size: 40,
+                  backgroundColor: Colors.blue,
+                ),
+                SizedBox(width: 20),
+                buildCircleButton(
+                  icon: Icons.skip_next,
+                  onPressed: _next,
+                  size: 40,
+                  backgroundColor: Colors.grey[700]!,
+                ),
+              ],
             ),
           ],
         ),
